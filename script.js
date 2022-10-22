267d394b84296a0f86783ff09e04399c295ef25d
Amol shelke <shelkeamol897@gmail.com>
2022-10-22 10:49:37 +0530

    Added the price card animation on hover

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
   item.style.cssText = `background-color: ${colors[i++]}`;
 });
 
-Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
-  item.onclick = () => {
-    item.parentElement.parentElement.classList.toggle("change");
-  };
-});
+// Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
+//   item.onclick = () => {
+//     item.parentElement.parentElement.classList.toggle("change");
+//   };
+// });
