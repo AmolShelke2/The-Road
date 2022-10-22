5901c6cadc604d6754da57e4b02887252d028209
Amol shelke <shelkeamol897@gmail.com>
2022-10-22 10:57:14 +0530

    Adding story section

--- script.js
+++ script.js
@@ -16,9 +16,3 @@ let i = 0;
 Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
   item.style.cssText = `background-color: ${colors[i++]}`;
 });
-
-// Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
-//   item.onclick = () => {
-//     item.parentElement.parentElement.classList.toggle("change");
-//   };
-// });
